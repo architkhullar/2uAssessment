42af32293af4ce017d61f386eb207c0624441702
Archit Khullar <akhullar@uncc.edu>
2019-08-24 18:49:20 -0400

    addimg build

---
+++ user_story2/build/precache-manifest.5102b3d3866b99e0dabd346d524b459e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7cb5695832cab3eabe835bebc79a34b6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7ab6fc5388241fd7dd00",
+    "url": "/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "dbdfa7ff4f2b21efadb0",
+    "url": "/static/js/2.9465d5d4.chunk.js"
+  },
+  {
+    "revision": "7ab6fc5388241fd7dd00",
+    "url": "/static/js/main.e7c0e715.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
